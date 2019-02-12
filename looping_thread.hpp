57cf9b2910bccc2de75851a156f25ff04c966633
Dugy <jan.dugacek@gmail.com>
2019-02-12 16:54:31 +0100

    Add a function to pause and unpause the inner thread

--- looping_thread.hpp
+++ looping_thread.hpp
@@ -13,61 +13,113 @@
 #include <chrono>
  
 class LoopingThread {
-	std::chrono::system_clock::duration _period;
-	std::function<void()> _routine;
-	std::timed_mutex _mutex;
-	std::unique_lock<std::timed_mutex> _lock;
-	std::thread _worker;
-
+	std::chrono::system_clock::duration period_;
+	std::function<void()> routine_;
+	std::timed_mutex pauseMutex_;
+	std::unique_lock<std::timed_mutex> pauseLock_;
+	std::mutex resumeMutex_;
+	std::unique_lock<std::mutex> resumeLock_;
+	std::thread worker_;
+	bool exiting_ = false;
+	bool paused_;
+	bool resetTimeOnPause_ = true;
+ 
 	inline void work() {
+		bool interrupted = false;
+		std::chrono::system_clock::time_point awakenAt = std::chrono::system_clock::now();
 		while(true) {
-			std::chrono::system_clock::time_point startedAt = std::chrono::system_clock::now();
-
-			_routine();
-
-			std::unique_lock<std::timed_mutex> lock(_mutex, std::try_to_lock);
-			if (lock.owns_lock())
-				break;
-			if (lock.try_lock_until(startedAt + _period))
-				break;
+			bool interrupted = false;
+			{
+				std::unique_lock<std::timed_mutex> lock(pauseMutex_, std::try_to_lock);
+				if (lock.owns_lock())
+					interrupted = true;
+				else {
+					if (awakenAt >= std::chrono::system_clock::now() && lock.try_lock_until(awakenAt))
+						interrupted = true;
+				}
+				if (resetTimeOnPause_) {
+					awakenAt + period_ = std::chrono::system_clock::now();
+					resetTimeOnPause_ = false;
+				}
+			}
+ 
+			if (interrupted) {
+				if (exiting_) break;
+				paused_ = true;
+				std::unique_lock<std::mutex> lock(resumeMutex_);
+				paused_ = false;
+			} else {
+				routine_();
+				awakenAt += period_;
+			}
 		}
 	}
 	public:
-
+ 
 	/*!
 	* \brief Default contructor, nothing is done if created this way
 	*/
-
-	inline LoopingThread() : _routine(nullptr) {
-
+ 
+	inline LoopingThread() : routine_(nullptr) {
+ 
 	}
-
+ 
 	/*!
 	* \brief Constructs the thread and starts running the routine periodically
 	* \param The calling period
 	* \param The function that is called periodically
+	* \param If the thread starts running or is paused until resume() is called
 	*/
-	inline LoopingThread(std::chrono::system_clock::duration period, std::function<void()> routine):
-		_period(period), _routine(routine), _lock(_mutex), _worker(&LoopingThread::work, this)
+	inline LoopingThread(std::chrono::system_clock::duration period, std::function<void()> routine, bool run = true):
+		period_(period),
+		routine_(routine),
+		pauseLock_(pauseMutex_, std::defer_lock),
+		resumeLock_(resumeMutex_),
+		worker_(&LoopingThread::work, this),
+		paused_(true),
+		resetTimeOnPause_(true)
 	{
-
+		if (run)
+			resume();
 	}
-
+ 
 	/*!
 	* \brief The destructor, interrupts the wait for another routine call, but waits for the routine to end if it's running
 	*/
 	inline ~LoopingThread() {
-		if (_routine) {
-			_lock.unlock();
-			_worker.join();
+		if (routine_) {
+			exiting_ = true;
+			pauseLock_.unlock();
+			worker_.join();
 		}
 	}
-
+ 
+	/*!
+	* \brief Pause the execution, will wait until the end of routine
+	*/
+	inline void pause(bool resetTime = true) {
+		if (routine_) {
+			resumeLock_.lock();
+			pauseLock_.unlock();
+			resetTimeOnPause_ = resetTime;
+		}
+	}
+ 
+	/*!
+	* \brief Resume paused execution
+	*/
+	inline void resume() {
+		if (routine_ && paused_) {
+			pauseLock_.lock();
+			resumeLock_.unlock();
+		}
+	}
+ 
 	/*!
 	* \brief Changes the period
 	* \param The new calling period
 	*/
 	inline void setPeriod(std::chrono::system_clock::duration newPeriod) {
-		_period = newPeriod;
+		period_ = newPeriod;
 	}
 };
--- looping_thread_test.cpp
+++ looping_thread_test.cpp
@@ -9,9 +9,15 @@ int main() {
 			std::this_thread::sleep_for(std::chrono::seconds(1));
 			std::cout << "(worker) Waited for 1s" << std::endl;
 		});
-		std::cout << "(main) Waiting for 5s" << std::endl;
-		std::this_thread::sleep_for(std::chrono::milliseconds(7200));
-		std::cout << "(main) Waited for 5s" << std::endl;
+		std::cout << "(main) Waiting for 3.6s" << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(3600));
+		std::cout << "(main) Pausing for 0.3s" << std::endl;
+		loop.pause(false);
+		std::this_thread::sleep_for(std::chrono::milliseconds(300));
+		loop.resume();
+		std::cout << "(main) Unpaused" << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(4300));
+		std::cout << "(main) Waited for 4.3s" << std::endl;
 	}
 	std::cout << "(main) Destroyed successfully" << std::endl;
 	return 0;
